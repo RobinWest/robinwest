8f5456a943527f50111d0df5132b737cb3d54b2b
rwest <robermak@gmail.com>
2014-07-27 18:10:31 +0100

    SVGs, yo!

--- build/var/www/www.robinwest.co.uk/index.php
+++ build/var/www/www.robinwest.co.uk/index.php
@@ -1,3 +1,7 @@
+<?php
+	$logo = 'asset/img/logo-simple-opt.svg';
+	$comingSoon = 'asset/img/comingsoon-opt.svg';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +11,14 @@
 	<link rel="stylesheet" href="asset/css/style.css">
 </head>
 <body>
+	<div class="pure-g">
+		<div id="logoContainer">
+			<? include($logo); ?>
+		</div>
+		<div id="comingSoonContainer">
+			<? include($comingSoon); ?>
+		</div>
+	</div>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="asset/js/main.js"></script>
