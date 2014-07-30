b4c3055ef0e9ae4eaf91650806309646ef574310
rwest <robermak@gmail.com>
2014-07-30 21:15:33 +0100

    put some responsive stuff in place

--- build/var/www/www.robinwest.co.uk/index.php
+++ build/var/www/www.robinwest.co.uk/index.php
@@ -1,22 +1,31 @@
 <?php
-	$logo = 'asset/img/logo-simple-opt.svg';
+	$logo 		= 'asset/img/logo-simple-opt.svg';
 	$comingSoon = 'asset/img/comingsoon-opt.svg';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<meta name="viewport" content="width=device-width, user-scalable=no">
 	<meta charset="UTF-8">
 	<title>Robin West</title>
 	<link rel="stylesheet" href="asset/css/pure.css">
 	<link rel="stylesheet" href="asset/css/style.css">
 </head>
 <body>
+	<? /*<div class="responsiveHelper">
+		<div class="responsiveHelper-sm">Small</div>
+		<div class="responsiveHelper-md">Medium</div>
+		<div class="responsiveHelper-lg">Large</div>
+		<div class="responsiveHelper-xl">X Large</div>
+	</div>*/?>
+	<div id="logoContainer">
+		<? include($logo); ?>
+	</div>
 	<div class="pure-g">
-		<div id="logoContainer">
-			<? include($logo); ?>
-		</div>
-		<div id="comingSoonContainer">
-			<? include($comingSoon); ?>
+		<div class="pure-u-1">
+			<div id="comingSoonContainer">
+				<? include($comingSoon); ?>
+			</div>
 		</div>
 	</div>
 
