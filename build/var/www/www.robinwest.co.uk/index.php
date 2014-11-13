2cc55573892ecb4c95ca6dad7b20bf8110124b14
rwest <robermak@gmail.com>
2014-11-13 20:54:38 +0000

    Bit of an overhaul. Need to remove pure, by the looks of things.

--- build/var/www/www.robinwest.co.uk/index.php
+++ build/var/www/www.robinwest.co.uk/index.php
@@ -1,7 +1,3 @@
-<?php
-	$logo 		= 'asset/img/logo-simple-opt.svg';
-	$comingSoon = 'asset/img/comingsoon-opt.svg';
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,22 +8,38 @@
 	<link rel="stylesheet" href="asset/css/style.css">
 </head>
 <body>
-	<? /*<div class="responsiveHelper">
-		<div class="responsiveHelper-sm">Small</div>
-		<div class="responsiveHelper-md">Medium</div>
-		<div class="responsiveHelper-lg">Large</div>
-		<div class="responsiveHelper-xl">X Large</div>
-	</div>*/?>
-	<div id="logoContainer">
-		<? include($logo); ?>
-	</div>
-	<div class="pure-g">
-		<div class="pure-u-1">
-			<div id="comingSoonContainer">
-				<? include($comingSoon); ?>
+	<section>
+		<div id="logo-container">
+			<svg viewBox="0 0 300 300">
+				<use xlink:href="asset/img/logo_trimmed.svg#logo"></use>
+			</svg>
+		</div>
+		<div id="information-container">
+			<svg viewBox="0 0 300 300">
+				<use xlink:href="asset/img/next_section.svg#next-section"></use>
+			</svg>
+		</div>
+	</section>
+	<section>
+		<div class="pure-g">
+			<div class="pure-u-1 nav-container">
+				<ul>
+					<li>About me</li>
+					<li><p>Previous work</p>Sign-Up.to</li>
+					<li><p>Previous work</p>Audiences</li>
+					<li><p>Previous work</p>SMS Manager</li>
+					<li><p>Previous work</p>labs.sign-up.to</li>
+				</ul>
+			</div>
+			<div class="pure-u-1 pure-u-lg-1-2 gallery-container">
+				<img src="asset/img/me.jpg" alt="">
+			</div>
+			<div class="pure-u-1 pure-u-lg-1-2 description-container">
+				<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique recusandae asperiores aliquid, laboriosam architecto. Animi delectus eligendi ipsam totam pariatur, beatae, doloribus dolorem natus quo et reiciendis itaque autem quia!</div>
+				<div class="tag"></div>
 			</div>
 		</div>
-	</div>
+	</section>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="asset/js/main.js"></script>
