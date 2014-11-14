09803a6e6b70ca9754310b0bbc8fc1b31bdbd4e7
rwest <robermak@gmail.com>
2014-11-14 19:10:11 +0000

    Progress. Consider adding angular for some basic templating and interaction

--- build/var/www/www.robinwest.co.uk/asset/js/main.js
+++ build/var/www/www.robinwest.co.uk/asset/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	// Lazy load in the higher res image
 	var animation,
 		bgImg = $('<img />').attr('src', 'asset/img/background.jpg').on('load', function(e){
-		$('body').css('background-image', 'url(\'asset/img/background.jpg\')');
+		$('.home').css('background-image', 'url(\'asset/img/background.jpg\')');
 	});
 
 	// Bind animation events
@@ -28,6 +28,15 @@ $(document).ready(function(){
 			.css('stroke-width', 0)
 			.css('stroke-dasharray', 0);
 	});
+
+
+	$('.navitem').on('click', function(e){
+		var $target = $(e.currentTarget);
+
+		$('.navitem.selected').removeClass('selected');
+
+		$target.addClass('selected');
+	})
 });
 
 function animateStroke($el){
--- build/var/www/www.robinwest.co.uk/index.php
+++ build/var/www/www.robinwest.co.uk/index.php
@@ -8,35 +8,52 @@
 	<link rel="stylesheet" href="asset/css/style.css">
 </head>
 <body>
-	<section>
-		<div id="logo-container">
+	<section class="home">
+		<div class="logo-container">
 			<svg viewBox="0 0 300 300">
 				<use xlink:href="asset/img/logo_trimmed.svg#logo"></use>
 			</svg>
 		</div>
-		<div id="information-container">
+		<div class="information-container">
 			<svg viewBox="0 0 300 300">
 				<use xlink:href="asset/img/next_section.svg#next-section"></use>
 			</svg>
 		</div>
 	</section>
 	<section>
-		<div class="pure-g">
-			<div class="pure-u-1 nav-container">
-				<ul>
-					<li>About me</li>
-					<li><p>Previous work</p>Sign-Up.to</li>
-					<li><p>Previous work</p>Audiences</li>
-					<li><p>Previous work</p>SMS Manager</li>
-					<li><p>Previous work</p>labs.sign-up.to</li>
-				</ul>
+		<div class="nav-container">
+			<div class="pure-g">
+				<div class="pure-u-1-5 navitem">
+					<p>Bio</p>
+					<h3>About me</h3>
+				</div>
+				<div class="pure-u-1-5 navitem">
+					<p>Previous work</p>
+					<h3>Sign-Up.to</h3>
+				</div>
+				<div class="pure-u-1-5 navitem">
+					<p>Previous work</p>
+					<h3>Audiences</h3>
+				</div>
+				<div class="pure-u-1-5 navitem">
+					<p>Previous work</p>
+					<h3>SMS Manager</h3>
+				</div>
+				<div class="pure-u-1-5 navitem">
+					<p>Previous work</p>
+					<h3>labs.sign-up.to</h3>
+				</div>
 			</div>
-			<div class="pure-u-1 pure-u-lg-1-2 gallery-container">
-				<img src="asset/img/me.jpg" alt="">
-			</div>
-			<div class="pure-u-1 pure-u-lg-1-2 description-container">
-				<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique recusandae asperiores aliquid, laboriosam architecto. Animi delectus eligendi ipsam totam pariatur, beatae, doloribus dolorem natus quo et reiciendis itaque autem quia!</div>
-				<div class="tag"></div>
+		</div>
+		<div class="content-container">
+			<div class="pure-g">
+				<div class="pure-u-1 pure-u-lg-1-2 gallery-container">
+					<img src="asset/img/me.jpg" alt="">
+				</div>
+				<div class="pure-u-1 pure-u-lg-1-2 description-container">
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique recusandae asperiores aliquid, laboriosam architecto. Animi delectus eligendi ipsam totam pariatur, beatae, doloribus dolorem natus quo et reiciendis itaque autem quia!</div>
+					<div class="corner"></div>
+				</div>
 			</div>
 		</div>
 	</section>
