b2afdc9a00522c8d721334f5ce6af1bbd6f1ca05
rwest <robermak@gmail.com>
2014-11-29 15:26:28 +0000

    Templated up the descriptions. Added a whole directive.

--- build/var/www/www.robinwest.co.uk/asset/js/main.js
+++ build/var/www/www.robinwest.co.uk/asset/js/main.js
@@ -25,12 +25,41 @@
 		}
 	});
 
-	app.controller('TabController', function(){
+	app.controller('ContentController', function(){
+		this.descriptions = [
+			{
+				id: 1,
+				name: 'About me',
+				tag: 'Bio',
+				imgSrc: 'asset/img/me.jpg',
+				templateSrc: 'asset/html/aboutme.html'
+			},{
+				id: 2,
+				name: 'Sign-Up.to',
+				tag: 'Previous work',
+				imgSrc: 'asset/img/sut-logo.jpg',
+				templateSrc: 'asset/html/sut.html'
+			},{
+				id: 3,
+				name: 'dev.sign-up.to',
+				tag: 'Previous Work',
+				imgSrc: 'asset/img/sut-dev.jpg',
+				templateSrc: 'asset/html/dev.html'
+			},{
+				id: 4,
+				name: 'Knit me a cake',
+				tag: 'Personal',
+				imgSrc: 'asset/img/knitmeacake.jpg',
+				templateSrc: 'asset/html/knitmeacake.html'
+			}
+		];
+
+		this.descLength = this.descriptions.length;
 		this.workTab = 1;
 
 		this.setTab = function(tab){
 			// bail if it's out of range
-			if(tab > 4 || tab < 1)
+			if(tab > this.descLength || tab < 1)
 				return;
 
 			this.workTab = tab;
@@ -39,5 +68,30 @@
 			return this.workTab === tab;
 		}
 	});
+
+	app.directive('content', ['$http', function($http){
+		var linker = function(scope, el, attrs){
+
+			$http.get(scope.source)
+				.success(function(data){
+					el.html(data);
+				})
+				.error(function(data, status){
+					console.log('Something went bad --');
+					console.log(data);
+					console.log(status);
+					// Hack for now
+					el.html('<p>Uh oh! Something went wrong. Sorry about that...</p>')
+				});
+		}
+
+		return {
+			restrict: 'E',
+			scope: {
+				source:'='
+			},
+			link: linker
+		};
+	}]);
 
 })();
--- build/var/www/www.robinwest.co.uk/index.php
+++ build/var/www/www.robinwest.co.uk/index.php
@@ -44,36 +44,24 @@
 			</div>
 		</div>
 	</section>
-	<section ng-controller="TabController as tab" id="work">
+	<section ng-controller="ContentController as content" id="work">
 		<div class="nav-container">
 			<div class="pure-g">
-				<div ng-click="tab.setTab(tab.workTab - 1)" class="pure-u-1-4 xl-hide">
-					<div ng-class="{ inactive:tab.isSet(1)}" class="nav-control">
+				<div ng-click="content.setTab(content.workTab - 1)" class="pure-u-1-4 xl-hide">
+					<div ng-class="{ inactive:content.isSet(1)}" class="nav-control">
 						<svg viewBox="0 0 300 300">
 							<use xlink:href="asset/img/svg/icons.svg#previous"></use>
 						</svg>
 					</div>
 				</div>
-			
-				<div ng-click="tab.setTab(1)" ng-class="{ selected:tab.isSet(1)}" class="pure-u-1-2 pure-u-xl-1-4 nav-item">
-					<p><span>Bio</span></p>
-					<h3>About me</h3>
-				</div>
-				<div ng-click="tab.setTab(2)" ng-class="{ selected:tab.isSet(2)}" class="pure-u-1-2 pure-u-xl-1-4 nav-item">
-					<p><span>Previous work</span></p>
-					<h3>Sign-Up.to</h3>
-				</div>
-				<div ng-click="tab.setTab(3)" ng-class="{ selected:tab.isSet(3)}" class="pure-u-1-2 pure-u-xl-1-4 nav-item">
-					<p><span>Previous work</span></p>
-					<h3>dev.sign-up.to</h3>
-				</div>
-				<div ng-click="tab.setTab(4)" ng-class="{ selected:tab.isSet(4)}" class="pure-u-1-2 pure-u-xl-1-4 nav-item">
-					<p><span>Personal</span></p>
-					<h3>Knit me a cake</h3>
-				</div>
 				
-				<div ng-click="tab.setTab(tab.workTab + 1)" class="pure-u-1-4 xl-hide">
-					<div ng-class="{ inactive:tab.isSet(4)}" class="nav-control">
+				<div ng-repeat="item in content.descriptions" ng-click="content.setTab(item.id)" ng-class="{ selected:content.isSet(item.id)}" class="pure-u-1-2 pure-u-xl-1-{{content.descLength}} nav-item">
+					<p><span>{{item.tag}}</span></p>
+					<h3>{{item.name}}</h3>
+				</div>
+
+				<div ng-click="content.setTab(content.workTab + 1)" class="pure-u-1-4 xl-hide">
+					<div ng-class="{ inactive:content.isSet(4)}" class="nav-control">
 						<svg viewBox="0 0 300 300">
 							<use xlink:href="asset/img/svg/icons.svg#next"></use>
 						</svg>
@@ -84,48 +72,16 @@
 		<div class="content-container">
 			<div class="pure-g">
 				<div class="pure-u-1 pure-u-xl-1-2 gallery-container">
-					<img ng-show="tab.isSet(1)" ng-src="asset/img/me.jpg">
-					<img ng-show="tab.isSet(2)" ng-src="asset/img/sut-logo.jpg">
-					<img ng-show="tab.isSet(3)" ng-src="asset/img/sut-dev.jpg">
-					<img ng-show="tab.isSet(4)" ng-src="asset/img/knitmeacake.jpg">
+					<img ng-repeat="item in content.descriptions" ng-show="content.isSet(item.id)" ng-src="{{item.imgSrc}}">
 				</div>
 				<div class="pure-u-1 pure-u-xl-1-2 information-container">
+
 					<article class="description-container">
-						<div ng-show="tab.isSet(1)" class="description">
-							<h1>Hi, I'm Robin&hellip;</h1>
-							<p>I'm a front end web developer specialising in UI and UX design based in Surrey, UK.</p>
-							<p>I'm an avid fan of most things creative; music, typography, photography, programming, illustration, design. I love trying out new techniques and throwing myself at things I don't know yet.</p>
-							<p>If I'm not doing one of the above I'm probably socialising, playing video games or watching old-school horror movies - usually with a beer in hand!</p>
-						</div>
-						
-						<div ng-show="tab.isSet(2)" class="description">
-							<h1>Sign-Up.to</h1>
-							<p>I am currently the User Experience Lead and Interaction team leader at Sign-Up.to. Since joining the Interaction team I have worked to improve the user experience of the Sign-Up.to platform and the processes used to develop our applications.</p>
-							<ul>
-								<li>Implemented a combination of design studio sessions, early paper prototyping, and iterative, usability-lead development for new applications.</li>
-								<li>Implemented analytics tools across specific areas of the platform to gain insight and smooth out common choke-points for new and existing users.</li>
-								<li>Using web technologies such as Backbone.js, Node.js, jQuery, HTML5, CSS3  to create snappy, easy to use applications with complex functionality.</li>
-								<li>Integration with Sign-Up.to's own RESTful <a href="http://dev.sign-up.to" target="_blank">PMAPI</a>.</li>
-							</ul>
-						</div>
-
-						<div ng-show="tab.isSet(3)" class="description">
-							<h1>dev.sign-up.to</h1>
-							<p>The documentation site for the <a href="http://dev.sign-up.to" target="_blank">Permission Marketing API</a> - Sign-Up.to's bespoke integration API.</p>
-							<ul>
-								<li>Design of various elements and sections of the site including the logo, <a href="https://dev.sign-up.to/documentation/reference/latest/guides/glossary/" target="_blank">glossary</a> and <a href="https://dev.sign-up.to/careers/" target="_blank">careers</a> section.</li>
-								<li>Working with the team to create a custom framework to support versioning, release logs and serve pages.</li>
-								<li>Continued maintenance and improvement of design and features.</li>
-								<li>Updating and writing documentation for existing and new endpoints.</li>
-							</ul>
-						</div>
-
-						<div ng-show="tab.isSet(4)" class="description">
-							<h1>Knit me a cake</h1>
-							<p>My friend <a href="https://twitter.com/LeanneBeale" target="_blank">@LeanneBeale</a> wanted a new logo and colour scheme created for her craft blog. We were both pleased with how the design turned out but recently a tough decision was made - to close the blog and, after a short break, make a <a href="http://thislittlespaceofmine.co.uk/" target="_blank">fresh start</a>.</p>
-							<p>I created the logo by using a chalk board and pens to give it a home made feel, then used Adobe Illustrator to vectorise and arrange the different elements.</p>
+						<div ng-repeat="item in content.descriptions" ng-show="content.isSet(item.id)" class="description">
+							<content source="item.templateSrc"></content>
 						</div>
 					</article>
+
 					<article class="contact-container">
 						<div class="pure-g">
 							<div class="pure-u-1">
