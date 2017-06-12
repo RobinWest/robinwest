178bd6a2dfa5957f93080d5b704744f8816cf399
Robin West <robin.west@kobas.co.uk>
2017-06-12 19:00:59 +0100

    Copy changes to the body and skills

--- app/containers/SkillsContainer.js
+++ app/containers/SkillsContainer.js
@@ -16,7 +16,7 @@ class SkillsContainer extends React.Component {
 				name: 'Front end',
 				icon: <EmbedSlashIcon />,
 				color: 'crimson',
-				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi officiis fuga porro expedita amet cupiditate voluptas tenetur incidunt commodi, vel exercitationem odio, deserunt natus tempora architecto dolor magnam quae unde.',
+				text: 'The user is always at the forefront of anything I do, and this has naturally led me towards specialising in front end development. With front end technology moving faster than ever, it\'s important to remember good practices and learn quickly. Learning new things is the reason I enjoy my work. I choose appropriate and modern tools and always try to make my code readable and clear for the next person who comes across it.',
 				tags: [
 					'HTML',
 					'CSS',
@@ -31,7 +31,7 @@ class SkillsContainer extends React.Component {
 				name: 'Back end',
 				icon: <TerminalIcon />,
 				color: 'moss',
-				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, voluptate, eum impedit praesentium mollitia dolorum commodi, placeat pariatur sit tenetur facilis repellat debitis consectetur excepturi natus aspernatur ratione deleniti, maxime.',
+				text: 'I beleive that the back end is an important part of the overall user experience and this has driven me to become comfortable with technologies and concepts involved in storing and retreiving data. REST APIs, relational databases, Apache setups and cloud computing consoles are all things I\'m used to working with. I am also comfortable setting up and using tooling for front end asset processing, just to make life a little easier for myself and the team.',
 				tags: [
 					'PHP',
 					'SQL',
@@ -41,14 +41,15 @@ class SkillsContainer extends React.Component {
 					'npm',
 					'Gulp',
 					'Grunt',
-					'Webpack'
+					'Webpack',
+					'AWS & GCE'
 				]
 			},{
 				active: false,
 				name: 'Design tools',
 				icon: <PaintRollerIcon />,
 				color: 'lake',
-				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi placeat temporibus dolore, ad, non tempora, voluptas mollitia obcaecati deserunt similique aperiam cum qui delectus. Nesciunt adipisci, dicta inventore velit error!',
+				text: 'I like to start projects with a sketch session to get as many different perspectives from as many stakeholders as possible. Then I usually take what I\'ve learned about the problem and try to create a prototype - either on paper, in code, or using mockup software. Finally I put together any required high-fidelity designs or assets using the Adobe suite or Sketch. This approach helps to separate the interface functionality from the shiny design so that the user\'s requirements are always kept a priority.',
 				tags: [
 					'Photoshop',
 					'Illustrator',
